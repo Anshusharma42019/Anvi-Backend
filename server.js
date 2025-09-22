3e0f57aadb90dc60dea84755699f819a1e0f8a52
Anshu sharma <156065826+Anshusharma42019@users.noreply.github.com>
2025-09-22 10:33:48 +0530

    newupdate

--- server.js
+++ server.js
@@ -7,7 +7,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://anvi-backend-v77n.vercel.app/",
+  ],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
